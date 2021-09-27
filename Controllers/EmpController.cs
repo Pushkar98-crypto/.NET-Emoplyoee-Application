23d5251773a070274c348bc6c6b08428160b5597
Pushkar98-crypto <pushkar.yadav@mail.vinove.com>
2021-09-27 17:48:25 +0530

    Intial

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCCrudApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace MVCCrudApplication.Controllers
+{
+    public class EmpController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmpController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var displaydata = _db.EmpTable.ToList();
+            return View(displaydata);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(NewEmpClass nec)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(nec);
+        }
+
+
+        public async Task <IActionResult>Edit(int?id)
+        {
+            if(id==null)
+            {
+                return RedirectToAction("index");
+            }
+
+            var getempdetail = await _db.EmpTable.FindAsync(id);
+            return View(getempdetail);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Edit (NewEmpClass nc)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Update(nc);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nc);
+        }
+
+
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("index");
+            }
+
+            var getempdetail = await _db.EmpTable.FindAsync(id);
+            return View(getempdetail);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("index");
+            }
+
+            var getempdetail = await _db.EmpTable.FindAsync(id);
+            return View(getempdetail);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var getempdetail = await _db.EmpTable.FindAsync(id);
+            _db.EmpTable.Remove(getempdetail);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/NewEmpClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCCrudApplication.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+        public int Empid { get; set; }
+
+
+        [Required(ErrorMessage ="Enter Employee Name")]
+        [Display (Name ="Employee Name")]
+        public String EmpName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Enter Email")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+
+
+
+        public String Email { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Enter Employee Age")]
+        [Display(Name = "Employee Age")]
+        [Range(20,80)]
+
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Salary")]
+        [Display(Name = "Salary")]
+        public int Salary { get; set; }
+
+
+    }
+}
