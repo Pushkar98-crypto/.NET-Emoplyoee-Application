6013a1f61fdea40446519a2b6938b0aca2aae18e
Pushkar98-crypto <pushkar.yadav@mail.vinove.com>
2021-09-28 11:45:21 +0530

    New changes

---
+++ Migrations/20210928033908_InitialDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCrudApplication.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmpTable",
+                columns: table => new
+                {
+                    Empid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpTable", x => x.Empid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmpTable");
+        }
+    }
+}
